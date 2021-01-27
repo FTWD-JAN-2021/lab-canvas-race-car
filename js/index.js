0082c6bc66163140485ab50d32bb85f9a8a2ba44
Conan Browne <Ventrik101@Gmail.com>
2021-01-27 15:44:36 -0500

    Conan

--- js/index.js
+++ js/index.js
@@ -1,7 +1,13 @@
-window.onload = () => {
-  document.getElementById('start-button').onclick = () => {
-    startGame();
-  };
+let canvas = document.querySelector("canvas")
+let ctx = canvas.getContext("2d")
+let carImg = new Image()
+carImg.src = "./images/car.png"
+let car = { x: 0, y: 0, w: 100, h: 200, img: carImg }
+// carImg.onload = animate
 
-  function startGame() {}
-};
+function animate() {
+  let gameLoop = requestAnimationFrame(animate)
+  console.log("animate")
+  ctx.drawImage(carImg, car.x, car.y, car.w, car.h)
+}
+animate()
