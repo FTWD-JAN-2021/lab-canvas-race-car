8e080b1a08dae0d7bca86344fcb5db5c8bbaaeac
Xavier Bergeron <xavbergeron@gmail.com>
2021-01-28 12:49:14 -0800

    Falling red boxes

--- js/index.js
+++ js/index.js
@@ -1,98 +1,126 @@
-let canvas = document.querySelector("canvas")
-let ctx = canvas.getContext("2d")
+let canvas = document.querySelector("canvas");
+let ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
 // roadImg.onload = animate
-let roadImg = new Image()
-roadImg.src = "./images/road.png"
-let road = {x: 0, y: 30, w: 450, h: 600, img: roadImg}
+let roadImg = new Image();
+roadImg.src = "./images/road.png";
+let road = { x: 0, y: 0, w: canvas.width, h: canvas.height, img: roadImg };
 
 // carImg.onload = animate
-let carImg = new Image()
-carImg.src = "./images/car.png"
+let carImg = new Image();
+carImg.src = "./images/car.png";
 
 class Car {
   constructor(x, y, w, h, img) {
-    this.x = x
-    this.y = y
-    this.w = w
-    this.h = h
-    this.img = img
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.img = img;
   }
   draw() {
-    ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
+    ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
   }
 }
 
-let toyota = new Car(canvas.width / 2 - 50 ,canvas.height-100, 50, 100, carImg) 
+let toyota = new Car(
+  canvas.width / 2 - 50,
+  canvas.height - 100,
+  50,
+  100,
+  carImg
+);
 
-window.onkeydown = function(e) {
-  console.log(e.key)
-    switch (e.key) {    
-      case "ArrowLeft":
-        // if (toyota.x >= 0)
-        toyota.x -= 30;
-       break;
-      case "ArrowRight":
-        // if ((toyota.x < canvas.width-toyota.w))
-        toyota.x += 30;
-        break
-      case "ArrowUp":
-        toyota.y -= 30;
-       break;
-      case "ArrowDown":
-        toyota.y += 30;
-        break
-    }
-}
+window.onkeydown = function (e) {
+  console.log(e.key);
+  switch (e.key) {
+    case "ArrowLeft":
+      // if (toyota.x >= 0)
+      toyota.x -= 30;
+      break;
+    case "ArrowRight":
+      // if ((toyota.x < canvas.width-toyota.w))
+      toyota.x += 30;
+      break;
+    case "ArrowUp":
+      toyota.y -= 30;
+      break;
+    case "ArrowDown":
+      toyota.y += 30;
+      break;
+  }
+};
 
-
-
-
-
-
-
+const startSpeed = 1;
+let speedMultiplier = 1;
 
 class Obstacle {
-  constructor (x,y,w,h, color) {
-    this.x = x
-    this.y = y
-    this.w = w
-    this.h = h
-    this.color = color 
+  constructor(x, y, w, h, color) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.color = color;
   }
   draw() {
-    ctx.fillStyle = this.color
-    ctx.fillRect (this.x, this.y, this.w, this.h) 
-    
+    this.y += startSpeed * speedMultiplier;
+
+    let { r, g, b } = this.color;
+
+    const pctDown = this.y / canvas.height;
+
+    // console.log(pctDown, this.y, canvas.height);
+    r = r + (255 - r) * pctDown;
+    g = g - g * pctDown;
+    b = b - b * pctDown;
+
+    ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+
+    ctx.fillRect(this.x, this.y, this.w, this.h);
   }
-  
 }
 
-let boxObstacle = new Obstacle(50, 50, 50, 50,"red" )
- 
-let boxes = []
+// let boxObstacle = new Obstacle(50, 50, 50, 50, "red");
+
+let boxes = [];
 
 function animate() {
-  let gameLoop = requestAnimationFrame(animate)
-  
-  ctx.clearRect(0,0,canvas.width, canvas.height)
-  ctx.drawImage(roadImg, road.x, road.y, road.w, road.h) 
-  toyota.draw()
-  boxObstacle.draw()
-  boxObstacle2.draw()
+  let gameLoop = requestAnimationFrame(animate);
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.drawImage(roadImg, road.x, road.y, road.w, road.h);
+  toyota.draw();
+  // boxObstacle.draw();
+  // boxObstacle2.draw();
   // box.draw()
-  for(box of boxes) {
-    box.draw()
-  } 
+  for (box of boxes) {
+    box.draw();
+  }
 }
 
-let boxObstacle2 = new Obstacle(100, 100, 50, 50,"gold")
-setInterval(()=>{
-  console.log("lo mein")
-let box = new Obstacle(canvas.width*Math.random(), 0, 50, 50, "green")
+function randomColor() {
+  const r = Math.floor(Math.random() * 255);
+  const g = Math.floor(Math.random() * 255);
+  const b = Math.floor(Math.random() * 255);
 
-boxes.push(box)
-console.log(boxes)
-}, 1000)
+  return { r, g, b };
+}
 
-animate()
+// let boxObstacle2 = new Obstacle(100, 100, 50, 50, "gold");
+setInterval(() => {
+  let box = new Obstacle(
+    canvas.width * Math.random(),
+    0,
+    50,
+    50,
+    randomColor()
+  );
+  speedMultiplier = Math.min(speedMultiplier + 0.15, 3);
+
+  boxes.push(box);
+}, 1000);
+
+animate();
