928d339f264fa5fa1cf63863a4e32d75b96d903b
Rastko <rastkodjurica@yahoo.com>
2021-01-28 16:51:38 -0500

    Rastko

--- js/index.js
+++ js/index.js
@@ -26,8 +26,24 @@ class Bullet {
   draw() {
     this.y--
     ctx.drawImage(this.img, this.x, this.y, this.w, this.h);
-  }
+    let i = 0
+    for (box of boxes) {
+      this.bulletCollDetection(box, this, i)
+      i++
+    }
 
+  }
+  bulletCollDetection(rect1, rect2, i) {
+    if (rect1.x < rect2.x + rect2.w &&
+      rect1.x + rect1.w > rect2.x &&
+      rect1.y < rect2.y + rect2.h &&
+      rect1.y + rect1.h > rect2.y) {
+      console.log('collision')
+      boxes.splice(i, 1)
+      this.w = 50
+      this.h = 50
+    }
+  }
 }
 
 class Car {
@@ -52,17 +68,18 @@ let toyota = new Car(
   carImg
 );
 
-function collisionDetection (rect1, rect2) {
+function collisionDetection(rect1, rect2) {
   if (rect1.x < rect2.x + rect2.w &&
     rect1.x + rect1.w > rect2.x &&
     rect1.y < rect2.y + rect2.h &&
     rect1.y + rect1.h > rect2.y) {
-     // collision detected!
-     //alert("Collision detected")
-     document.getElementById("gameOver").innerHTML = "GAME OVER"
-     cancelAnimationFrame(gameLoop)
- }
+    // collision detected!
+    //alert("Collision detected")
+    document.getElementById("gameOver").innerHTML = "GAME OVER"
+    cancelAnimationFrame(gameLoop)
+  }
 }
+
 
 window.onkeydown = function (e) {
   console.log(e.key);
@@ -81,8 +98,8 @@ window.onkeydown = function (e) {
     case "ArrowDown":
       toyota.y += 30;
       break;
-     case " ":
-       bullets.push(new Bullet(toyota.x, toyota.y, 10, 10, elon))
+    case " ":
+      bullets.push(new Bullet(toyota.x, toyota.y, 10, 10, elon))
   }
 };
 const bullets = []
@@ -115,14 +132,14 @@ class Obstacle {
 
     ctx.fillRect(this.x, this.y, this.w, this.h);
 
-    collisionDetection (this, toyota)
+    collisionDetection(this, toyota)
   }
 }
 
 // let boxObstacle = new Obstacle(50, 50, 50, 50, "red");
 
 let boxes = [];
-let gameLoop; 
+let gameLoop;
 function animate() {
   gameLoop = requestAnimationFrame(animate);
 
@@ -134,6 +151,7 @@ function animate() {
   // box.draw()
   for (box of boxes) {
     box.draw();
+
   }
   for (bullet of bullets) {
     bullet.draw();
